3b960463156c775802fee763a2c5ae7c7114201c
Bob King <bob@getready1.com>
2020-09-22 18:22:41 -0400

    stub some funcs

---
+++ app/ec2.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+import boto3
+
+
+def get_aws_config():
+    # TODO: stub: get_aws_config()
+    return config
+
+
+def make_session():
+    # TODO: stub: make_session()
+    return
+
+
+def launch_ec2_instance(params):
+    # TODO: stub: launch_ec2_instance(params)
+    return
+
+
+def terminate_ec2_instance(params):
+    # TODO: stub: terminate_ec2_instance(params)
+    return
+
+
+def get_running_ec2_instance(params):
+    # TODO: stub: get_running_ec2_instance(params)
+    return
