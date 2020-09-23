9f30367ef95961cb59f44934df6670ce69e41f0a
Bob King <bob@getready1.com>
2020-09-22 21:37:50 -0400

    add get_ec2_instances()

--- app/ec2.py
+++ app/ec2.py
@@ -1,7 +1,9 @@
 import os
 import json
+from pprint import pprint
 
 import boto3
+import botocore.exceptions
 
 
 def get_aws_config():
@@ -15,8 +17,14 @@ def get_aws_config():
 
 
 def make_session():
-    # TODO: stub: make_session()
-    return
+    try:
+        config = get_aws_config()
+        new_session = boto3.session.Session(
+            aws_access_key_id=config["aws_access_key_id"],
+            aws_secret_access_key=config["aws_secret_access_key"],)
+        return new_session
+    except Exception as e:
+        return e
 
 
 def launch_ec2_instance(params):
@@ -24,15 +32,35 @@ def launch_ec2_instance(params):
     return
 
 
+def get_ec2_instances(filters=[], dry_run=False, pagination_config={"MaxItems": 20, "PageSize": 20}, **kwargs):
+    try:
+        session = make_session()
+        client = session.client('ec2')
+        paginator = client.get_paginator('describe_instances')
+        response_iterator = paginator.paginate(
+            Filters=filters,
+            DryRun=dry_run,
+            PaginationConfig=pagination_config
+        )
+    except botocore.exceptions.ParamValidationError as error:
+        raise ValueError(
+            'The parameters you provided are incorrect: {}'.format(error))
+    except botocore.exceptions.ClientError as error:
+        raise error
+
+    return response_iterator
+
+
 def terminate_ec2_instance(params):
     # TODO: stub: terminate_ec2_instance(params)
     return
 
 
-def get_running_ec2_instance(params):
-    # TODO: stub: get_running_ec2_instance(params)
-    return
-
-
-my_conf = get_aws_config()
-print(my_conf)
+# params = {}
+# filters = [{"Name": "tag:Organization", "Values": ["Ready-1"]}]
+# dry_run = False
+# pagination_config = {"MaxItems": 20, "PageSize": 20}
+# page_iterator = get_ec2_instances(filters, dry_run, pagination_config)
+page_iterator = get_ec2_instances()
+for page in page_iterator:
+    pprint(page, indent=1)
---
+++ app/tests/test_ec2.py
@@ -0,0 +1,21 @@
+import pytest
+
+import ec2
+
+
+def test_get_aws_config():
+    conf = ec2.get_aws_config()
+    assert "aws_access_key_id" in conf.keys()
+
+
+def test_make_session():
+    session = ec2.make_session()
+    assert session.region_name == 'us-east-1'
+
+
+def test_get_ec2_instances():
+    page_iterator = ec2.get_ec2_instances()
+    pages = []
+    for page in page_iterator:
+        pages.append(page)
+    assert pages[0]["ResponseMetadata"]["HTTPStatusCode"] == 200
