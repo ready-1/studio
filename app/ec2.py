3e6d5f7060fabbb4bd77016d2b963ce6653cf5ce
Bob King <bob@getready1.com>
2020-09-23 11:02:57 -0400

    add docstrings & stub create_launch_template

--- app/ec2.py
+++ app/ec2.py
@@ -8,6 +8,13 @@ import botocore.exceptions
 
 
 def get_aws_config():
+    """Read the aws credentials file
+
+    Read the aws_config.json file and return a dict
+
+    Returns:
+        dict: dictionary of the aws config parameters
+    """
     try:
         with open('../aws_config.json', 'r') as stream:
             # TODO: change to use BASEDIR
@@ -18,14 +25,19 @@ def get_aws_config():
 
 
 def make_session():
+    """Instantiate an boto3 session object
+
+    Returns:
+        new_session (obj): boto3 session
+    """
     try:
         config = get_aws_config()
         new_session = boto3.session.Session(
             aws_access_key_id=config["aws_access_key_id"],
             aws_secret_access_key=config["aws_secret_access_key"],)
-        return new_session
     except Exception as e:
         return e
+    return new_session
 
 
 def launch_ec2_instances(params):
@@ -79,6 +91,21 @@ def launch_ec2_instances(params):
 
 
 def get_ec2_instances(filters=[], dry_run=False, pagination_config={"MaxItems": 20, "PageSize": 20}, **kwargs):
+    """Get a list of active ec2 instances
+
+    Args:
+        filters (list, optional): filters dictionaries. Defaults to [].
+        dry_run (bool, optional): Should the operation be a dry run? Defaults to False.
+        pagination_config (dict, optional): Customize the paginator. Defaults to {"MaxItems": 20, "PageSize": 20}.
+
+    Raises:
+        ValueError: If the parameters are wrong types.
+        error: Catch all.
+
+    Returns:
+        dict: Dictionary of instances and HTTP parameters
+    """
+    # TODO: Change the arguments to be parameters like launch_ec2_instances()
     try:
         session = make_session()
         client = session.client('ec2')
@@ -93,13 +120,19 @@ def get_ec2_instances(filters=[], dry_run=False, pagination_config={"MaxItems": 
             'The parameters you provided are incorrect: {}'.format(error))
     except botocore.exceptions.ClientError as error:
         raise error
-
     return response_iterator
 
 
 def terminate_ec2_instance(params):
-    # TODO: stub: terminate_ec2_instance(params)
+    # STUB: terminate_ec2_instance(params)
+    # TODO: add test for terminate_ec2_instance
     return
+
+
+def create_launch_template():
+    # STUB: create_launch_template
+    # TODO: add test for create_launch_template
+    pass
 
 
 # configure instance_params
