5e36bd402d937f916350c85c3f221622ab1a9b0d
Bob King <bob@getready1.com>
2020-09-23 10:47:56 -0400

    add launch_ec2_instances()

--- app/ec2.py
+++ app/ec2.py
@@ -1,6 +1,7 @@
 import os
 import json
 from pprint import pprint
+import datetime
 
 import boto3
 import botocore.exceptions
@@ -27,9 +28,54 @@ def make_session():
         return e
 
 
-def launch_ec2_instance(params):
-    # TODO: stub: launch_ec2_instance(params)
-    return
+def launch_ec2_instances(params):
+    """Launch EC2 instances with given parameters
+
+    Returns:
+        response (list): List of ec2 instance objects
+    Args:
+        params (dict): Per specification at https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html?highlight=create_instances#EC2.ServiceResource.create_instances
+
+        {
+        "ImageId": 'string',
+        "InstanceType": 'string',
+        "MaxCount": 1,
+        "MinCount": 1,
+        "Monitoring": {
+            'Enabled': False
+        },
+        "SecurityGroupIds": [
+            'string',
+        ],
+        "SubnetId": 'string',
+        "DryRun": False,
+        "EbsOptimized": False,
+        "InstanceInitiatedShutdownBehavior": 'stop',
+        "TagSpecifications": [
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'Name', 'Value': 'Studio-1A Linux Tiny'},
+                    {'Key': 'Job', 'Value': 'Testing'},
+                    {'Key': 'Organization', 'Value': 'Ready-1'},
+                    {'Key': 'Studio', 'Value': 'Studio-1A'},
+                    {'Key': 'Purpose', 'Value': 'Testing'},
+                ]
+            },
+        ],
+    }
+    """
+
+    try:
+        session = make_session()
+        ec2_connection = session.resource('ec2')
+        # launch instance
+        response = ec2_connection.create_instances(**params)
+    except FileNotFoundError as error:
+        return f'ERROR: {error}'
+    except botocore.exceptions.ClientError as error:
+        return f'ERROR: {error}'
+    return response
 
 
 def get_ec2_instances(filters=[], dry_run=False, pagination_config={"MaxItems": 20, "PageSize": 20}, **kwargs):
@@ -54,3 +100,38 @@ def get_ec2_instances(filters=[], dry_run=False, pagination_config={"MaxItems": 
 def terminate_ec2_instance(params):
     # TODO: stub: terminate_ec2_instance(params)
     return
+
+
+# configure instance_params
+instance_params = {
+    "ImageId": 'ami-00514a528eadbc95b',
+    "InstanceType": 't2.micro',
+    "MaxCount": 1,
+    "MinCount": 1,
+    "Monitoring": {
+        'Enabled': False
+    },
+    "SecurityGroupIds": [
+        'sg-0e87825359f9b21ff',
+    ],
+    "SubnetId": 'subnet-08a94fe90f894ef3b',
+    "DryRun": False,
+    "EbsOptimized": False,
+    "InstanceInitiatedShutdownBehavior": 'stop',
+    "TagSpecifications": [
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {'Key': 'Name', 'Value': 'Studio-1A Linux Tiny'},
+                {'Key': 'Job', 'Value': 'Testing'},
+                {'Key': 'Organization', 'Value': 'Ready-1'},
+                {'Key': 'Studio', 'Value': 'Studio-1A'},
+                {'Key': 'Purpose', 'Value': 'Testing'},
+            ]
+        },
+    ],
+}
+
+result = launch_ec2_instances(instance_params)
+# pprint(result[0].tags[0]["Value"])
+pprint(result)
--- app/tests/test_ec2.py
+++ app/tests/test_ec2.py
@@ -21,3 +21,38 @@ def test_get_ec2_instances():
     assert pages[0]["ResponseMetadata"]["HTTPStatusCode"] == 200
 
     # TODO: add better tests for get_ec2_instances()
+
+
+def test_launch_ec2_instance():
+    instance_params = {
+        "ImageId": 'ami-00514a528eadbc95b',
+        "InstanceType": 't2.micro',
+        "MaxCount": 1,
+        "MinCount": 1,
+        "Monitoring": {
+            'Enabled': False
+        },
+        "SecurityGroupIds": [
+            'sg-0e87825359f9b21ff',
+        ],
+        "SubnetId": 'subnet-08a94fe90f894ef3b',
+        "DryRun": True,
+        "EbsOptimized": False,
+        "InstanceInitiatedShutdownBehavior": 'stop',
+        "TagSpecifications": [
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {'Key': 'Name', 'Value': 'Studio-1A Linux Tiny'},
+                    {'Key': 'Job', 'Value': 'Testing'},
+                    {'Key': 'Organization', 'Value': 'Ready-1'},
+                    {'Key': 'Studio', 'Value': 'Studio-1A'},
+                    {'Key': 'Purpose', 'Value': 'Testing'},
+                ]
+            },
+        ],
+    }
+
+    result = ec2.launch_ec2_instances(instance_params)
+
+    assert 'Request would have succeeded, but DryRun flag is set.' in result
